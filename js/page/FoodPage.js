9fedecde5458397a6b8716b6895e0d976b55f554
HEITOR <heitor7@163.com>
2017-08-25 00:27:20 +0800

    load Data

--- js/page/FoodPage.js
+++ js/page/FoodPage.js
@@ -5,9 +5,23 @@ import {
     View,
     Image,
     StyleSheet,
+    Text,
+    TextInput,
 } from 'react-native';
+import DataRepository from '../remote/DataRepository';
+
+const URL = 'https://api.github.com/search/repositories?q='
+const QUERY_STR = '&sort=stars';
 
 class FoodPage extends React.Component {
+
+    constructor() {
+        super();
+        this.dataRespository = new DataRepository();
+        this.state = {
+            result: ''
+        }
+    }
 
     static navigationOptions = {
         headerTitle: 'Food',
@@ -23,22 +37,44 @@ class FoodPage extends React.Component {
         }
     };
 
+    onLoad() {
+        let url = this.getUrl(this.text);
+        this.dataRespository.fetchNetRepository(url)
+            .then(result => {
+                this.setState({
+                    result: JSON.stringify(result)
+                })
+            })
+            .catch(error => {
+                this.setState({
+                    result: JSON.stringify(error)
+                })
+            });
+    }
+
+    getUrl(key) {
+        return URL + key + QUERY_STR;
+    }
+
     render() {
         return (
-            <View style={styles.food}></View>
+            <View>
+                <Text style={styles.tips} onPress={() => this.onLoad()}>获取数据</Text>
+                <TextInput style={{height: 20, borderWidth: 1}} onChangeText={text => this.text = text}/>
+                <Text style={{height: 500}}>{this.state.result}</Text>
+            </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    food: {
-        flex: 1,
-        backgroundColor: 'orange',
-    },
     icon: {
         width: 26,
         height: 26,
     },
+    tips: {
+        fontSize: 29,
+    }
 });
 
 export default FoodPage;
